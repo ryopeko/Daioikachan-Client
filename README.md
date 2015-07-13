255a704602502ecfb3e4cac27ba62d8a85a02c78
ryopeko <ryopeko+free@gmail.com>
2015-07-13 18:29:14 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Daioikachan::Client - It's new $module
+
+# SYNOPSIS
+
+    use Daioikachan::Client;
+
+# DESCRIPTION
+
+Daioikachan::Client is ...
+
+# LICENSE
+
+Copyright (C) ryopeko.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+ryopeko <ryopeko+free@gmail.com>
---
+++ lib/Daioikachan/Client.pm
@@ -0,0 +1,41 @@
+package Daioikachan::Client;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Daioikachan::Client - TBD
+
+=head1 SYNOPSIS
+
+    use Daioikachan::Client;
+
+    TBD
+
+=head1 DESCRIPTION
+
+TBD
+
+=head1 LICENSE
+
+Copyright (C) ryopeko.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+ryopeko E<lt>ryopeko@gmail.comE<gt>
+
+=cut
+
