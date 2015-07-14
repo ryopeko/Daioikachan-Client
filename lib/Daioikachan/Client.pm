68fb917ca6f8a90a440b5c2f2a46657ef2a23443
ryopeko <ryopeko+free@gmail.com>
2015-07-14 18:52:31 +0900

    First implementation

--- lib/Daioikachan/Client.pm
+++ lib/Daioikachan/Client.pm
@@ -1,11 +1,73 @@
 package Daioikachan::Client;
-use 5.008001;
+
 use strict;
 use warnings;
 
 our $VERSION = "0.01";
 
+use Furl;
+use Carp;
 
+sub new {
+    my ($class, $args) = @_;
+
+    my $ua = Furl->new(
+        agent   => 'daioikachan-agent',
+        timeout => 10,
+        defined $args->{ua_options} ? %{$args->{ua_options}} : (),
+    );
+
+    my $default_channel      = (defined $args->{default_channel} && ($args->{default_channel} ne '')) ? $args->{default_channel} : '#notify';
+    my $daioikachan_endpoint = (defined $args->{daioikachan_endpoint} && ($args->{daioikachan_endpoint} ne '')) ? $args->{daioikachan_endpoint} : die 'Undefined daioikachan_endpoint';
+
+    my $header = $args->{header};
+
+    return bless {
+        ua     => $ua,
+        header => $header,
+        default_channel      => $default_channel,
+        daioikachan_endpoint => $daioikachan_endpoint,
+    }, $class;
+}
+
+sub notice {
+    my ($self, $args) = @_;
+
+    my $message = (defined $args->{message} && ($args->{message} ne '')) ? $args->{message} : die 'Undefined message';
+
+    return $self->_send({
+        channel => $args->{channel},
+        message => $message,
+        type    => 'notice',
+    });
+}
+
+sub privmsg {
+    my ($self, $args) = @_;
+
+    my $message = (defined $args->{message} && ($args->{message} ne '')) ? $args->{message} : die 'Undefined message';
+
+    return $self->_send({
+        channel => $args->{channel},
+        message => $message,
+        type    => 'privmsg',
+    });
+}
+
+sub _send {
+    my ($self, $args) = @_;
+
+    my $channel = (defined $args->{channel} && ($args->{channel} ne '')) ? $args->{channel} : $self->{default_channel};
+
+    return $self->{ua}->post(
+        $self->{daioikachan_endpoint} . $args->{type},
+        $self->{header},
+        {
+            channel => $channel,
+            message => $args->{message},
+        },
+    );
+}
 
 1;
 __END__
